ee02d0bdbe7ba51c45cdc46e25df3283372a707a
sam <857330534@qq.com>
2015-06-18 23:08:43 +0800

    branch for test. add new Page, se ka.

--- ColorCard/gen/mytest/colorcard/R.java
+++ ColorCard/gen/mytest/colorcard/R.java
@@ -33,13 +33,20 @@ public final class R {
         public static final int tabselected=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int TableRow01=0x7f090006;
+        public static final int TextView01=0x7f090008;
+        public static final int View01=0x7f090007;
+        public static final int action_settings=0x7f090009;
+        public static final int scrollView1=0x7f090003;
+        public static final int tableRow1=0x7f090004;
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090002;
+        public static final int view1=0x7f090005;
     }
     public static final class layout {
         public static final int activity_color_card=0x7f030000;
+        public static final int color_sample=0x7f030001;
     }
     public static final class menu {
         public static final int color_card=0x7f080000;
