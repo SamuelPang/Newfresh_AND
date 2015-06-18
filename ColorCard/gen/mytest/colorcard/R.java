04bc116d882e9d71b531f4b72e157ce3baa80c5f
sam <857330534@qq.com>
2015-06-18 23:41:59 +0800

    add page jiansuo

--- ColorCard/gen/mytest/colorcard/R.java
+++ ColorCard/gen/mytest/colorcard/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int black=0x7f040002;
+        public static final int gray=0x7f040003;
         public static final int ivory=0x7f040001;
         public static final int white=0x7f040000;
     }
@@ -36,17 +37,21 @@ public final class R {
         public static final int TableRow01=0x7f090006;
         public static final int TextView01=0x7f090008;
         public static final int View01=0x7f090007;
-        public static final int action_settings=0x7f090009;
+        public static final int action_settings=0x7f09000c;
+        public static final int button1=0x7f090009;
+        public static final int editText1=0x7f09000a;
         public static final int scrollView1=0x7f090003;
         public static final int tableRow1=0x7f090004;
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090002;
         public static final int view1=0x7f090005;
+        public static final int view2=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_color_card=0x7f030000;
         public static final int color_sample=0x7f030001;
+        public static final int color_search=0x7f030002;
     }
     public static final class menu {
         public static final int color_card=0x7f080000;
