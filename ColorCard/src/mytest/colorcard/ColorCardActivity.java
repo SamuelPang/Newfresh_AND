2727f87bbd04a19c4ec748172e9649fc6d0fc52c
sam <857330534@qq.com>
2015-06-19 00:54:28 +0800

    page1 selected action

--- ColorCard/src/mytest/colorcard/ColorCardActivity.java
+++ ColorCard/src/mytest/colorcard/ColorCardActivity.java
@@ -1,23 +1,62 @@
 package mytest.colorcard;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ColorCardActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color_card);
-    }
+	private LinearLayout sampleTab = null;
+	private LinearLayout searchTab = null;
+	private LinearLayout identifyTab = null;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_color_card);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.color_card, menu);
-        return true;
-    }
-    
+		sampleTab = (LinearLayout) findViewById(R.id.sampleTab);
+		searchTab = (LinearLayout) findViewById(R.id.searchTab);
+		identifyTab = (LinearLayout) findViewById(R.id.identifyTab);
+
+		sampleTab.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				sampleTab.setBackgroundResource(R.drawable.tabselected);
+				TextView tView = (TextView) sampleTab.getChildAt(0);
+				tView.setTextColor(getResources().getColor(R.color.gray));
+			}
+		});
+		searchTab.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				searchTab.setBackgroundResource(R.drawable.tabselected);
+				TextView tView = (TextView) searchTab.getChildAt(0);
+				tView.setTextColor(getResources().getColor(R.color.gray));
+			}
+		});
+		identifyTab.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				searchTab.setBackgroundResource(R.drawable.tabselected);
+				TextView tView = (TextView) searchTab.getChildAt(0);
+				tView.setTextColor(getResources().getColor(R.color.gray));
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.color_card, menu);
+		return true;
+	}
+
 }
